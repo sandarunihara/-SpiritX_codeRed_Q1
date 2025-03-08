7673a7c1f52868b22349f2ed6c70879852c05005
NIHARA W.A.S <sandarunihara56@gmail.com>
2025-03-08 23:27:27 +0530

    add user model and implement signup and login pages with routing

---
+++ backend/models/UserModel.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    confirmPassword: {
+        type: String,
+        required: true,
+    },
+});
+
+const User= mongoose.model('User', UserSchema);
+
+export default User;
